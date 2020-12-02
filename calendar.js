f51d77550480327618a01399d55a9fd34fd53084
Mikko Heiskanen <morbusg@trioptimum.com>
2020-12-02 15:00:15 +0200

    fix week#, fix week padding, fixed days# in month

--- calendar.js
+++ calendar.js
@@ -1,40 +1,50 @@
-import {chunk, concat, cons, fold, map} from './curried-functions.js'
-import {append, elem} from './dom-helpers.js'
-
-const indexSucc = (_, i) => i + 1
+import {B} from './curried/combinators.js'
+import {concat, fold, map} from './curried/array.js'
+import {car, chunk, cons} from './curried/array-extra.js'
+import {append, elem} from './curried/dom-helpers.js'
+import {eq} from './curried/comparators.js'
 
 const timeElem = attrs => elem('time')(attrs)
 
+const isFirstZero = B(eq(0))(car)
+
 const pad = num => String(num).padStart(2, '0')
 
-const monthWeeks = year => month =>
-  chunk
+const monthWeeks = year => month => {
+  const firstWeekday = new Date(year, month).getDay()
+  const lastDay = new Date(new Date(year, month + 1) - 1).getDate()
+
+  // NOTE week start on monday
+  const padAmount = eq(firstWeekday)(0) ? 6 : firstWeekday - 1
+
+  return chunk
     (7)
     (concat
       (cons
         (_ => 0)
-        (new Array(Math.max(new Date(year, month).getDay() - 1, 0))))
+        (new Array(padAmount)))
       (cons
-        (indexSucc)
-        (new Array(new Date(year, month, -0).getDate()))))
+        ((_, i) => i + 1)
+        (new Array(lastDay))))
+}
 
 const yearDays = year =>
   map
     (monthWeeks(year))
     (cons
-      (indexSucc)
+      ((_, i) => i)
       (new Array(12)))
 
 export const yearElems = year => {
   let weekNum = 0
 
-  const monthElems = (yearElem, month, monthNum) =>
+  const monthElems = (yearElem, month, monthIndex) =>
     append
       (yearElem)
       (fold
         (timeElem({
           className: 'month'
-          , dateTime: `${year}-${pad(monthNum + 1)}`
+          , dateTime: `${year}-${pad(monthIndex + 1)}`
         }))
         (weekElems)
         (month))
@@ -43,7 +53,10 @@ export const yearElems = year => {
     append
       (monthElem)
       (fold
-        (timeElem({className: 'week', dateTime: `${year}-W${++weekNum}`}))
+        (timeElem({
+          className: 'week'
+          , dateTime: `${year}-W${isFirstZero(week) ? weekNum : ++weekNum}`
+        }))
         ((weekElem, day) =>
           append
             (weekElem)
@@ -59,5 +72,5 @@ export const yearElems = year => {
   return fold
     (timeElem({className: 'year', dateTime: year}))
     (monthElems)
-    (yearDays(year))
+    (yearDays(Number(year)))
 }
